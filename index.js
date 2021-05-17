a31a9e14c344c09a903ca955d53b6ee6cfb2890d
Venom <76788803+itsme-ven0m@users.noreply.github.com>
2021-05-17 22:01:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('ODM3ODY4MzA2MTMxNDUyMDA0.YIyzqw.eTYfhCwAvztLn0_bCbr8ez-EKyE');
+client.login('');
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
