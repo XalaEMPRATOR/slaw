22eae8d56a7b4e5903670f3aa48fcef61c9c8fbf
Venom <76788803+itsme-ven0m@users.noreply.github.com>
2021-05-17 13:22:40 +0300

    Update invite.js

--- others/invite.js
+++ others/invite.js
@@ -15,7 +15,7 @@ module.exports = {
     //react with approve emoji
     message.react("<a:setting:813404135181385759>");
     //send the invite embed
-    message.reply(new MessageEmbed().setColor("#146DF6").setTitle("").setDescription("<a:arbelearbele:780373854039572501> **[Click Here](https://discord.com/api/oauth2/authorize?client_id=837868306131452004&permissions=8&scope=bot) to invite the bot.**")
+    message.reply(new MessageEmbed().setColor("#146DF6").setTitle("").setDescription("<a:emoji_17:839459192631722025> **[Click Here](https://discord.com/api/oauth2/authorize?client_id=837868306131452004&permissions=8&scope=bot) to invite the bot.**")
     .setFooter(""));
   }
 }
